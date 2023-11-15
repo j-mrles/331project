c4e36ebf79c33c72297f8c1f264f56622a5e0648
Javier Morales <102753009+j-mrles@users.noreply.github.com>
2023-11-15 12:10:43 -0500

    Add files via upload

---
+++ GreenLightGameController.java
@@ -0,0 +1,77 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private Label DifficlultyLabel;
+
+    @FXML
+    private Button EasyLevelButton;
+
+    @FXML
+    private Label GuessLabel;
+
+    @FXML
+    private TextField GuessTextField;
+
+    @FXML
+    private Button HardBLevelButton;
+
+    @FXML
+    private Label HintLabel;
+
+    @FXML
+    private TextArea HintTextArea;
+
+    @FXML
+    private Button MedimLevelButton;
+
+    @FXML
+    private TextArea ResultWindowArea;
+
+    @FXML
+    private ImageView SkyPicture;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button checkAnswerButton;
+
+    @FXML
+    private ImageView fungreenimage1;
+
+    @FXML
+    private ImageView fungreenimage2;
+
+    @FXML
+    private ImageView fungreenimage3;
+
+    @FXML
+    void CheckButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EasyButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void HardButtonPressed(ActionEvent event) {
+
+    }
+
+    @FXML
+    void MediumButtonPressed(ActionEvent event) {
+
+    }
+
+}
